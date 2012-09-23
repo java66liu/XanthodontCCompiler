fbfc68d221c61d0b937c25b9bb64c05a73933406
xanthodont <xanthodont@hotmail.com>
2012-09-23 18:47:31 +0800

    测试提交

---
+++ src/com/xanthodont/ccompiler/test/Default.java
@@ -0,0 +1,13 @@
+package com.xanthodont.ccompiler.test;
+
+public class Default {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
