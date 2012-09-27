5dcd10f907ddb86708c3fd5ea9bd4580dc2da2fe
xanthodont <xanthodont@hotmail.com>
2012-09-27 20:11:35 +0800

    添加C语言的字母表类AlphabetC

---
+++ src/com/xanthodont/ccompiler/structuresystem/AlphabetC.java
@@ -0,0 +1,25 @@
+/************************************************************************
+* 版权所有 (C)2012,xanthodont 
+* http://www.cnblogs.com/hbq-fczzw/						 
+*																		 
+* 文件名称：AlphabetC.java							                     
+* 内容摘要：C语言中所有用到的字符集                                                             
+* 其它说明：                                                             
+* 当前版本：v1.0												         
+* 作	者：	   xanthodont											         
+* 完成日期：2012-9-27                      									     
+************************************************************************/
+package com.xanthodont.ccompiler.structuresystem;
+
+public class AlphabetC {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+			
+	}
+
+}
--- src/com/xanthodont/ccompiler/test/Default.java
+++ src/com/xanthodont/ccompiler/test/Default.java
@@ -7,6 +7,8 @@ public class Default {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int i = 1 - - - 2 + - + + -38;
+		System.out.print(i);
 	}
 
 }
