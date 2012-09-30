5831165863442d057fd42dab1f2e9a573492ca36
xanthodont <xanthodont@hotmail.com>
2012-09-30 13:48:44 +0800

    增加Token记号类的实现代码，并添加说明信息

--- src/com/xanthodont/ccompiler/structuresystem/Token.java
+++ src/com/xanthodont/ccompiler/structuresystem/Token.java
@@ -3,7 +3,7 @@
 * http://www.cnblogs.com/hbq-fczzw/						 
 *																		 
 * 文件名称：Token.java							                     
-* 内容摘要：                                                             
+* 内容摘要： 记号类，作为其他具体记号类的基类                                                            
 * 其它说明：                                                             
 * 当前版本：v1.0												         
 * 作	者：	   xanthodont											         
@@ -13,5 +13,17 @@ package com.xanthodont.ccompiler.structuresystem;
 
 public class Token 
 {
+	/** 记号类别字段 */
+	public final int tag;
 	
+	/**
+	 * 构造方法，初始化tag字段
+	 * @param tag 
+	 * 		      记号类别，具体值为com.xanthodont.ccompiler.structuresystem。Tag类的静态final字段。
+	 * 		  
+	 */
+	public Token(int tag)
+	{
+		this.tag = tag;
+	}
 }
