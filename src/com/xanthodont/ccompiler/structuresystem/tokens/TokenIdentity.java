d15b7e8fbaba6387c027c242d8f870f810b2fde7
xanthodont <xanthodont@hotmail.com>
2012-10-01 20:19:08 +0800

    1.修改TokenInteger类的注释说明 2.添加一个表示标识符的记号类TokenIdentity

--- src/com/xanthodont/ccompiler/structuresystem/tokens/TokenIdentity.java
+++ src/com/xanthodont/ccompiler/structuresystem/tokens/TokenIdentity.java
@@ -2,8 +2,8 @@
 * 版权所有 (C)2012,xanthodont 
 * http://www.cnblogs.com/hbq-fczzw/						 
 *																		 
-* 文件名称：TokenNumber.java							                     
-* 内容摘要： 表示整型数记号的类                                                            
+* 文件名称：TokenIdentity.java							                     
+* 内容摘要： 代表标识符的记号类                                                            
 * 其它说明：                                                             
 * 当前版本：v1.0												         
 * 作	者：	   xanthodont											         
@@ -14,21 +14,22 @@ package com.xanthodont.ccompiler.structuresystem.tokens;
 import com.xanthodont.ccompiler.structuresystem.Tag;
 import com.xanthodont.ccompiler.structuresystem.Token;
 /**
- * 继承自Token类，表示整型数的记号类
+ * 表示标识符的记号类
  * @author xanthondont
  *
  */
-public class TokenInteger extends Token{
-	/** 整型数的值 */
-	private int _value;
+public class TokenIdentity extends Token{
+
+	/** 标识符串 */
+	private String _lexeme;
 	/**
 	 * 构造方法
-	 * @param value 整型数的值
+	 * @param lexeme 标识符串
 	 */
-	public TokenInteger(int value)
+	public TokenIdentity(String lexeme)
 	{
-		super(Tag.INTEGER);
-		this._value = value;
+		super(Tag.IDENTITY);
+		this._lexeme = lexeme;
 	}
 	
 	/**
